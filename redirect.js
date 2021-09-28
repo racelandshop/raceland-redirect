0f7a2e90a3a1cd45fe6eaa8dcc276980626e72df
zroger49 <zroger499@gmail.com>
2021-09-28 11:05:44 +0100

    Test

--- redirect.js
+++ redirect.js
@@ -5,13 +5,14 @@ var user;
 var width; 
 var height; 
 
-function run(){
-    loadVars()
+
+function getDeviceInfo(){
+  user = Hass.user.name;
+  width = window.innerWidth;
+  height = window.innerHeight;
 }
 
-
-
-function loadVars() {
+function run() {
     Root = document.querySelector("home-assistant");
     Root = Root && Root.shadowRoot;
     Root = Root && Root.querySelector("home-assistant-main");
@@ -26,11 +27,20 @@ function loadVars() {
     }
     catch { }
     if (Root) {
-        user = Hass.user.name;
-        width = window.innerWidth;
-        height = window.innerHeight;
+      getDeviceInfo();
+      // window.location.href = 'http://192.168.1.142:8123/homekit-infused/main_menu';
+      history.replaceState(null, "", "http://192.168.1.142:8123/homekit-infused/main_menu");
+      const event = new Event("location-changed", {
+        bubbles: true,
+        composed: true,
+      });
+      event.detail = { replace: true };
+      this.dispatchEvent(event);
+
     }
+
+    console.log("the User is ", user, " and the screen size is ", width, "x", height);
   }
 
+
 run(); 
-console.log("the User is ", user, " and the screen size is ", width, "x", height);
